4073011dfeaace40b43481ac75b04b9cdfdbe4f5
Arkady <kornienko@outlook.com>
2025-12-20 23:47:40 +1000

    Add files via upload

---
+++ NET4/x64/sqlite_reg.cmd
@@ -0,0 +1,5 @@
+Rem ========================================
+Rem  REGISTRATION of library sqlite.net.dll
+Rem ========================================
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe sqlite.net.dll /codebase
---
+++ NET4/x64/sqlite_unreg.cmd
@@ -0,0 +1,5 @@
+Rem ==========================================
+Rem  UNREGISTRATION of library sqlite.net.dll
+Rem ==========================================
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe sqlite.net.dll /unregister
