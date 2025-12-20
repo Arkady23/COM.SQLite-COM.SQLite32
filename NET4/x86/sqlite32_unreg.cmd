84f8b0adaf6459cc696853c33d67b9025a0ae82b
Arkady <kornienko@outlook.com>
2025-12-20 23:46:00 +1000

    Add files via upload

---
+++ NET4/x86/sqlite32_unreg.cmd
@@ -0,0 +1,5 @@
+Rem ============================================
+Rem  UNREGISTRATION of library sqlite32.net.dll
+Rem ============================================
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe sqlite32.net.dll /unregister
